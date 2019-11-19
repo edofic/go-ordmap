73ab39787a041755869205eb701a4382872a71ea
Andraz Bajt <andraz@bajt.me>
2019-11-19 20:31:19 +0100

    Clean up pack code

--- internal/pack/main.go
+++ internal/pack/main.go
@@ -10,34 +10,23 @@ import (
 	"os"
 )
 
-func main() {
-	source := flag.String("source", "./source.go", "Source file to pack")
-	dest := flag.String("dest", "./packed.go", "Destination to write packed code to")
-	name := flag.String("name", "packed", "Variable name to use in generated code")
-	pkg := flag.String("pkg", "main", "Package name to use")
-	// TODO compression
-	flag.Parse()
-
-	content, err := ioutil.ReadFile(*source)
-	if err != nil {
-		log.Println("Cannot read file", *source)
-		os.Exit(1)
-	}
-
+func compress(raw []byte) []byte {
 	var b bytes.Buffer
 	w := gzip.NewWriter(&b)
-	w.Write(content)
+	w.Write(raw)
 	w.Close()
-	compressed := b.Bytes()
+	return b.Bytes()
+}
 
+func formatOutput(pkg, name string, compressed []byte) []byte {
 	buf := bytes.NewBuffer(make([]byte, 0))
-	fmt.Fprintf(buf, "package %s\n\n", *pkg)
+	fmt.Fprintf(buf, "package %s\n\n", pkg)
 	fmt.Fprintf(buf, "import (\n")
 	fmt.Fprintln(buf, "\t\"bytes\"")
 	fmt.Fprintln(buf, "\t\"compress/gzip\"")
 	fmt.Fprintln(buf, "\t\"io/ioutil\"")
 	fmt.Fprint(buf, ")\n\n")
-	fmt.Fprintf(buf, "func %v() []byte {\n", *name)
+	fmt.Fprintf(buf, "func %v() []byte {\n", name)
 	fmt.Fprintf(buf, "\tbuf := bytes.NewBuffer([]byte{")
 	for i, char := range compressed {
 		if i > 0 {
@@ -57,5 +46,21 @@ func main() {
 	}
 	return decompressed`)
 	fmt.Fprintf(buf, "\n}")
-	ioutil.WriteFile(*dest, buf.Bytes(), 0644)
+	return buf.Bytes()
+}
+
+func main() {
+	source := flag.String("source", "./source.go", "Source file to pack")
+	dest := flag.String("dest", "./packed.go", "Destination to write packed code to")
+	name := flag.String("name", "packed", "Variable name to use in generated code")
+	pkg := flag.String("pkg", "main", "Package name to use")
+	// TODO compression
+	flag.Parse()
+
+	content, err := ioutil.ReadFile(*source)
+	if err != nil {
+		log.Println("Cannot read file", *source)
+		os.Exit(1)
+	}
+	ioutil.WriteFile(*dest, formatOutput(*pkg, *name, compress(content)), 0644)
 }
