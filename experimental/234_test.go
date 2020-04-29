5f611ac73e9943c34bb7ea4b6fd352967f5b1bee
Andraz Bajt <andraz@bajt.me>
2020-04-29 17:33:18 +0200

    Add basic model test

--- experimental/234_test.go
+++ experimental/234_test.go
@@ -1,10 +1,91 @@
 package ordmap
 
 import (
+	"fmt"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+type Model234 struct {
+	t     *testing.T
+	tree  *Node234
+	elems []int
+}
+
+func NewModel234(t *testing.T) *Model234 {
+	m := &Model234{
+		t:     t,
+		elems: []int{},
+	}
+	m.checkInvariants()
+	return m
+}
+
+func (m *Model234) checkInvariants() {
+	fmt.Println(m.tree.visual())
+	m.checkNodesValidity()
+	m.checkBalance()
+	m.checkElements()
+}
+
+func (m *Model234) checkNodesValidity() {
+	var step func(*Node234)
+	step = func(n *Node234) {
+		if n == nil {
+			return
+		}
+		for i := int(n.order + 1); i < len(n.keys); i++ {
+			require.Equal(m.t, 0, n.keys[i])
+			require.Nil(m.t, n.subtrees[i+1])
+		}
+		children := 0
+		for i := 0; i <= int(n.order); i++ {
+			if n.subtrees[i] != nil {
+				children += 1
+			}
+		}
+		if n.leaf {
+			require.Equal(m.t, children, 0)
+			return
+		}
+		require.Greater(m.t, children, 0)
+		for i := 0; i <= int(n.order); i++ {
+			step(n.subtrees[i])
+		}
+	}
+	step(m.tree)
+}
+
+func (m *Model234) checkBalance() {
+	var depth func(*Node234) int
+	depth = func(n *Node234) int {
+		if n == nil {
+			return 0
+		}
+		if n.leaf {
+			return 1
+		}
+		d1 := depth(n.subtrees[0])
+		for _, n = range n.subtrees[1 : n.order+1] {
+			require.Equal(m.t, d1, depth(n))
+		}
+		return d1
+	}
+	depth(m.tree)
+}
+
+func (m *Model234) checkElements() {
+	require.Equal(m.t, m.elems, m.tree.Keys())
+}
+
+func (m *Model234) Insert(key int) {
+	m.tree = m.tree.Insert(key)
+	m.elems = append(m.elems, key)
+	m.elems = sort.IntSlice(m.elems)
+	m.checkInvariants()
+}
 
 func TestBasic234(t *testing.T) {
 	N := 7
@@ -42,4 +123,12 @@ func TestStealLeft(t *testing.T) {
 	}
 	n = n.Remove(20)
 	require.Equal(t, []int{0, 1, 2, 10}, n.Keys())
+}
+
+func TestModel(t *testing.T) {
+	N := 10
+	m := NewModel234(t)
+	for i := 0; i < N; i++ {
+		m.Insert(i)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/edofic/go-ordmap
 
-go 1.12
+go 1.13
 
-require github.com/stretchr/testify v1.3.0
+require github.com/stretchr/testify v1.5.1
