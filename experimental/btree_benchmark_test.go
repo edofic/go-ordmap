6942f32495a6f147b4645a169adfe29ef50bf1e8
Andraz Bajt <andraz.bajt@leanix.net>
2021-12-18 10:12:42 +0100

    Copy over experimental b-tree

---
+++ experimental/btree_benchmark_test.go
@@ -0,0 +1,63 @@
+package ordmap
+
+import (
+	"fmt"
+	"testing"
+)
+
+//go:generate go run github.com/edofic/go-ordmap/cmd/gen -impl avl  -name AvlIntSet -key int -less "<" -value struct{} -target ./avl_int_set.go -pkg ordmap
+
+func BenchmarkComparison(b *testing.B) {
+	for _, M := range []int{10, 100, 1000, 10000, 100000} {
+		b.Run(fmt.Sprintf("%v", M), func(b *testing.B) {
+			b.Run("avl", func(b *testing.B) {
+				var tree *AvlIntSet
+				for i := 0; i < M; i++ {
+					tree = tree.Insert(i, struct{}{})
+				}
+				b.Run("Get", func(b *testing.B) {
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						tree.Get(5)
+					}
+				})
+				b.Run("Insert", func(b *testing.B) {
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						tree.Insert(M+1, struct{}{})
+					}
+				})
+				b.Run("Remove", func(b *testing.B) {
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						tree.Remove(i % M)
+					}
+				})
+			})
+			b.Run("btree", func(b *testing.B) {
+				var tree *OrdMap
+				for i := 0; i < M; i++ {
+					tree = tree.Insert(intKey(i), struct{}{})
+				}
+				b.Run("Get", func(b *testing.B) {
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						tree.Get(intKey(5))
+					}
+				})
+				b.Run("Insert", func(b *testing.B) {
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						tree.Insert(intKey(M+1), struct{}{})
+					}
+				})
+				b.Run("Remove", func(b *testing.B) {
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						tree.Remove(intKey(i % M))
+					}
+				})
+			})
+		})
+	}
+}
